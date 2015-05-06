54b896ac6870b7a9d962e972f1283d374e62f936
Vincent Zorge <vzorge@gmail.com>
2015-05-06 23:16:04 +0200

    css aanpassingen

--- src/main/webapp/HypotheekController.js
+++ src/main/webapp/HypotheekController.js
@@ -99,4 +99,17 @@ angular.module('hypotheekApp', []).controller('HypotheekController', ['$http', '
     Chart.defaults.global.animationEasing = "linear";
 
     self.initialize();
-}]);
+}]).filter('sumByKey', function() {
+    return function(data, key) {
+        if (typeof(data) === 'undefined' || typeof(key) === 'undefined') {
+            return 0;
+        }
+
+        var sum = 0;
+        for (var i = data.length - 1; i >= 0; i--) {
+            sum += parseInt(data[i][key]);
+        }
+
+        return sum;
+    };
+});
